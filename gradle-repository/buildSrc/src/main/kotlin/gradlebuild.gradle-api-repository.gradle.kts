6c176feefeb80663012396ea840f7b5440dbe696
Stefan Wolf <wolf@gradle.com>
2022-03-24 18:24:09 +0100

    Publish a fat Gradle API jar
    instead of the individual JARs. That simplifies the publishing
    configuration, since we only have one publication now.

--- gradle-repository/buildSrc/src/main/kotlin/gradlebuild.gradle-api-repository.gradle.kts
+++ gradle-repository/buildSrc/src/main/kotlin/gradlebuild.gradle-api-repository.gradle.kts
@@ -36,17 +36,9 @@ val artifactType = Attribute.of("artifactType", String::class.java)
 val runtimeAttribute = objects.named<Usage>(Usage.JAVA_RUNTIME)
 
 val gradleDistribution by configurations.creating
-val gradleClasspath by configurations.creating {
-    extendsFrom(gradleDistribution)
-    attributes.attribute(artifactType, "gradle-classpath")
-}
 val gradleJarsConfiguration by configurations.creating {
     extendsFrom(gradleDistribution)
     attributes.attribute(artifactType, "gradle-libs-dir")
-}
-val gradleArtifactListConfiguration by configurations.creating {
-    extendsFrom(gradleDistribution)
-    attributes.attribute(artifactType, "gradle-artifact-list")
 }
 
 dependencies {
@@ -56,32 +48,24 @@ dependencies {
         from.attribute(artifactType, "zip")
         to.attribute(artifactType, "gradle-libs-dir")
     }
-    registerTransform(CreateGradleArtifactList::class) {
-        parameters {
-            gradleVersion.set(gradleApiRepository.gradleVersion)
-        }
-        from.attribute(artifactType, "gradle-libs-dir")
-        to.attribute(artifactType, "gradle-artifact-list")
-    }
 }
 
-val thirdPartyJarsPath = "third-party"
-val gradleJarsPath = "gradle-jars"
-
-val resolveJars by tasks.registering(Sync::class) {
-    from(gradleJarsConfiguration) {
-        into(thirdPartyJarsPath)
-        exclude("gradle-*")
-    }
-    from(gradleJarsConfiguration) {
-        into(gradleJarsPath)
-        include("gradle-*")
-    }
-    into(project.buildDir.resolve("libs"))
+val createShadedGradleApiJar by tasks.registering(Jar::class) {
+    from(gradleJarsConfiguration.asFileTree.elements
+        .map { jars -> jars
+            .filter { it.asFile.name.startsWith("gradle-") }
+            .map { zipTree(it) }
+        }
+    )
+    destinationDirectory.set(project.layout.buildDirectory.dir("libs"))
+    archiveFileName.set(gradleApiRepository.gradleVersion.map { "gradle-api-${it}.jar" })
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
 
 afterEvaluate {
-    val gradleApiJarGroupId = "org.gradle"
+
+    // We need to configure publishing in afterEvaluate,
+    // since the repository URL and the MavenPublication.version aren't providers.
 
     publishing {
         repositories {
@@ -90,37 +74,11 @@ afterEvaluate {
                 url = uri(gradleApiRepository.repoLocation)
             }
         }
-    }
-
-    if (gradle.startParameter.taskNames.any { it.contains("publish") }) {
-        val artifactList = gradleArtifactListConfiguration.singleFile
-        val gradleArtifacts = artifactList.readLines().filter(String::isNotBlank)
-        val gradleVersion = gradleApiRepository.gradleVersion.get()
-        gradleArtifacts.forEach { artifactName ->
-            publishing.publications.create<MavenPublication>(artifactName) {
-                groupId = gradleApiJarGroupId
-                artifactId = artifactName
-                version = gradleApiRepository.gradleVersion.get()
-                artifact(resolveJars.map { it.destinationDir.resolve("$gradleJarsPath/$artifactName-$gradleVersion.jar") })
-            }
-        }
-        publishing.publications.create<MavenPublication>("gradle-api") {
-            groupId = gradleApiJarGroupId
+        publishing.publications.create<MavenPublication>("gradleApi") {
+            groupId = "org.gradle"
             artifactId = "gradle-api"
             version = gradleApiRepository.gradleVersion.get()
-            pom {
-                packaging = "pom"
-                withXml {
-                    val dependencies = asNode().appendNode("dependencies")
-                    gradleArtifacts.forEach { artifactName ->
-                        dependencies.appendNode("dependency").apply {
-                            appendNode("groupId", "org.gradle")
-                            appendNode("artifactId", artifactName)
-                            appendNode("version", gradleVersion)
-                        }
-                    }
-                }
-            }
+            artifact(createShadedGradleApiJar)
         }
     }
 }
